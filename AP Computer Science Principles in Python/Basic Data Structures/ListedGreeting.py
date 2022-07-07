43e74ea8abb783ebdbcf22b2daa20e8811459d79
Fereydoun Memarzanjany <Thraetaona@outlook.com>
2022-07-08 01:57:51 +0300

    Delete ListedGreeting.py

--- AP Computer Science Principles in Python/Basic Data Structures/ListedGreeting.py
+++
@@ -1,8 +0,0 @@
-# fill in this function to greet the user!
-def greeting(user_info):
-    
-    info_list = user_info.split()
-    
-    
-    
-    return "Hello, " + str(info_list[0]) + "! I also enjoy " + str(info_list[2]) + "!"
